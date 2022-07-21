f4e39c5799ec866dd95ff85868fedc9dcb1dc833
Seven Tsai <mvpscottjon@gmail.com>
2022-07-21 18:33:47 +0800

    add models

---
+++ Pets/Models/PhotoAndTitleInfo.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAndTitleInfo.swift
+//  Pets
+//
+//  Created by Seven on 2022/7/19.
+//
+
+import UIKit
+
+struct PhotoAndTitleInfo {
+    var image: UIImage?
+    var title: String?
+    var subtitle: String?
+}
