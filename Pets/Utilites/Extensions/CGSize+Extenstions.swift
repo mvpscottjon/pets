1d07cd58f2db4a24a1fa484545f5c9563a72700d
Seven Tsai <mvpscottjon@gmail.com>
2022-08-07 00:43:53 +0800

    chore: update CGSize and UIEdgeInsets extensions (#18)

---
+++ Pets/Utilites/Extensions/CGSize+Extenstions.swift
@@ -0,0 +1,14 @@
+//
+//  CGSize+Extenstions.swift
+//  Pets
+//
+//  Created by Seven on 2022/8/5.
+//
+
+import UIKit
+
+extension CGSize {
+    init(size: CGFloat) {
+        self.init(width: size, height: size)
+    }
+}
