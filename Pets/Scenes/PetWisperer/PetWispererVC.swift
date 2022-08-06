36aa03e3bdc1f4eeb19659ac008a4349d02a309c
Seven Tsai <mvpscottjon@gmail.com>
2022-08-07 00:35:06 +0800

    update PerWispererVC UI and tweak PetWispererVM

--- Pets/Scenes/PetWisperer/PetWispererVC.swift
+++ Pets/Scenes/PetWisperer/PetWispererVC.swift
@@ -6,10 +6,38 @@
 //
 
 import UIKit
+import RxSwift
+import SnapKit
 
 class PetWispererVC: UIViewController {
 
+    private enum Constants {
+        static let stackViewInsets = UIEdgeInsets(horizontalInsets: 10.0, verticalInsets: 5.0)
+        static let backgroundColor = UIColor.background
+        static let stackViewSpacing = 20.0
+    }
+    
+    // MARK: - Properties
+    
     private let vm: PetWispererVM
+    private let bag = DisposeBag()
+    
+    
+    // MARK: - Views
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = Constants.stackViewSpacing
+
+        return stackView
+    }()
+    
     
     // MARK: - Initializers
     
@@ -25,7 +53,104 @@ class PetWispererVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupViews()
+        bindViewModels()
     }
 
+    // MARK: - Setups
+    
+    private func setupViews(){
+        view.backgroundColor = Constants.backgroundColor
+        
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        scrollView.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.width.equalToSuperview().inset(Constants.stackViewInsets)
+        }
+    }
+    
+    // MARK: - Bindings
+    
+    private func bindViewModels() {
+        vm.state.sections
+            .drive(with: self, onNext: { `self`, sections in
+                self.updateSectionViews(sections)
+            })
+            .disposed(by: bag)
+    }
+    
+    private func updateSectionViews(_ sections: [PetWispererVM.Section]) {
+        stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        sections.forEach {
+            stackView.addArrangedSubview(makeSectionView(section: $0))
+        }
+    }
+    
+    // MARK: - Factories
+    
+    private func makeSectionView(section: PetWispererVM.Section) -> UIView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        
+        let sectionView: UIView
+        switch section {
+        case .doctor(let doctors):
+            sectionView = makeDoctorView(doctors: doctors)
+        case .wisperer(let wisperers):
+            sectionView = makeWispererView(wisperers: wisperers)
+        }
+        
+        stackView.addArrangedSubview(sectionView)
+        
+        sectionView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        
+        return stackView
+    }
+    
+    private func makeDoctorView(doctors: [Doctor]) -> UIView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10.0
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "醫生"
+        titleLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+        stackView.addArrangedSubview(titleLabel)
+        
+        doctors.forEach {
+            let view = DoctorView(doctor: $0)
+            stackView.addArrangedSubview(view)
+        }
+        
+        return stackView
+    }
+    
+    private func makeWispererView(wisperers: [Wisperer]) -> UIView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10.0
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "溝通師"
+        titleLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        stackView.addArrangedSubview(titleLabel)
+        
+        wisperers.forEach {
+            let view = WispererView(wisperer: $0 )
+            stackView.addArrangedSubview(view)
+        }
+        
+        return stackView
+    }
 }
--- Pets/Scenes/PetWisperer/PetWispererVM.swift
+++ Pets/Scenes/PetWisperer/PetWispererVM.swift
@@ -6,142 +6,42 @@
 //
 
 import Foundation
-import Alamofire
 import RxSwift
+import RxCocoa
 
 struct PetWispererVM {
-
-    init() {
-    test()
-        
+    
+    enum Section {
+        case doctor([Doctor])
+        case wisperer([Wisperer])
     }
     
-    private func test()  {
-        // APIRequest
-        
-        WispererAPIRequest().send(completion: { rs in
-            switch rs {
-            case .success(let res):
-                Logger.log("res", res)
-            case .failure(let error):
-                Logger.log("error", error)
-            }
-        })
-        
-//        let req = WispererAPIRequest()
-//        API.sendRequest(req, completion: { rs in
-//            switch rs {
-//            case .success(let res):
-//                Logger.log("res")
-//            case .failure(let error):
-//                Logger.log("error")
-//            }
-//        })
-        
-        
-//        let urlString = "https://od.moi.gov.tw/od/data/api/EA28418E-8956-4790-BAF4-C2D3988266CC?$format=json"
-//        guard let url = URL(string: urlString) else { return .empty() }
+    struct State {
+        let sections: Driver<[Section]>
+    }
+    
+    // MARK: - Properties
+    
+    let state: State
+    
+    private let sections = BehaviorRelay<[Section]>(value: [])
 
-        // normal
-//        let request = AF.request(url, method: .get, parameters: nil).response { response in
-//            switch response.result {
-//            case .success(let data):
-//                guard let data = data else { return }
-//                let json = try? JSONSerialization.jsonObject(with: data)
-//                DebugLogger.log(item: "SUCCESS", json)
-//            case .failure(let error):
-//                DebugLogger.log(item: "ERROR", error)
-//            }
-//        }
+    init() {
+        self.state = State(sections: sections.asDriver())
         
-        // APIManager
-//        APIManager.shared.call(endPointType: <#T##EndPointType#>, handler: <#T##((DataResponse<Decodable, AFError>) -> Void)##((DataResponse<Decodable, AFError>) -> Void)##(DataResponse<Decodable, AFError>) -> Void#>)
-//        APIManager.shared.call(endPointType: EndPoint.getWisperer, handler: {  (rs: (AFDataResponse<Wisperer>) -> Void) in
-//            
-//        })
-
-//        APIManager.shared.call(endPointType: EndPoint.getWisperer, handler: { (DataResponse<Wisperer, AFError>) -> Void in
-//            
-//        })
+        // mock data
+        let doctorImageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWS_XhkZQcIkI-XLMCpNZNk54m_Z5rPOEsAQ&usqp=CAU"
+        let mockDoctors = ["1", "2", "3", "4", "5"].map {
+            Doctor(name: $0, title: $0, skill: $0, language: $0, imagUrl: URL(string: doctorImageURL))
+        }
         
-//        APIManager.shared.call(endPointType: EndPoint.getWisperer) {  (value: [Wisperer]?, error)  in
-//
-//            print("here:", value, error)
-//        }
-//
-        // Worker
+        let wisperperURL = "https://yummytw.com/wp-content/uploads/20220511190123_26.jpeg"
+        let mockWisperers = ["1", "2", "3", "4", "5"].map {
+            Wisperer(name: $0, title: $0, charge: $0, language: $0, imagUrl: URL(string: wisperperURL))
+        }
         
-//        Worker.GetWisperer().request { rs in
-////            DebugLogger.log(item: "看WORKER RS", rs)
-//            switch rs.result {
-//            case .success(let data):
-//                Logger.log("SUCCESS:", data)
-//            case .failure(let error):
-//                Logger.log("FAILED:", error)
-//            }
-//        }
-        
-        
-        
-        // rx
-        
-        
-//        Observable<[String: Any]>.create { observer in
-//            DebugLogger.log(item: "有跑這ㄌㄧ")
-//
-//            let urlString = "https://od.moi.gov.tw/od/data/api/EA28418E-8956-4790-BAF4-C2D3988266CC?$format=json"
-//            guard let url = URL(string: urlString) else { return observer as! Disposable }
-//
-//            let request = AF.request(url, method: .get, parameters: nil).response { response in
-//                switch response.result {
-//                case .success(let data):
-//
-//                    guard let data = data,
-//                          let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
-//                    else { return }
-//
-//                    observer.onNext(json)
-//                    observer.onCompleted()
-//                    DebugLogger.log(item: "SUCCESS", json)
-//                case .failure(let error):
-//                    DebugLogger.log(item: "ERROR", error)
-//                    observer.onError(error)
-//                }
-//            }
-//
-//            return Disposables.create {
-//                request.cancel()
-//            }
-//        }
-        
-
-
-        
-        
-        
-//        AF.request(<#T##convertible: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Encodable?#>, encoder: <#T##ParameterEncoder#>, headers: <#T##HTTPHeaders?#>, interceptor: <#T##RequestInterceptor?#>, requestModifier: <#T##Session.RequestModifier?##Session.RequestModifier?##(inout URLRequest) throws -> Void#>).response(completionHandler: <#T##(AFDataResponse<Data?>) -> Void#>)
-        
-//        API.GetWisperer().request(completion: { res in
-//            switch res.result {
-//            case .success(let data):
-//                guard let data = data else { return }
-//                let json = try? JSONSerialization.jsonObject(with: data)
-//                DebugLogger.log(item: "SUCCESS", json)
-//            case .failure(let error):
-//                DebugLogger.log(item: "ERROR", error)
-//            }
-//
-//        })
-//
-//            .request().response { res in
-//            switch res.result {
-//            case .success(let data):
-//                guard let data = data else { return }
-//                let json = try? JSONSerialization.jsonObject(with: data)
-//                DebugLogger.log(item: "SUCCESS", json)
-//            case .failure(let error):
-//                DebugLogger.log(item: "ERROR", error)
-//            }
-//        }
+        var mockSections = [Section.doctor(mockDoctors)]
+        mockSections.append(Section.wisperer(mockWisperers))
+        sections.accept(mockSections)
     }
 }
